b8fbec4d938f88160af5b31dec165a2a45213224
Churkin Aleksey <lekhachurkin@gmail.com>
2025-11-06 21:59:11 +0300

    now works on windows

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 #include "daScript/das_common.h"
 
 int main() {
-    auto guard = das::daScriptEnvironmentGuard();
-    das::TextPrinter tout;
     NEED_MODULE(Module_BuiltIn)
+    das::Module::Initialize();
+
+    das::TextPrinter tout;
     das::ModuleGroup dummyGroup;
     auto program = compileDaScript("hello.das",das::make_smart<das::FsFileAccess>(),tout,dummyGroup,{});
     auto pctx = das::SimulateWithErrReport(program, tout);
     auto fnVec = pctx->findFunctions("main");
     pctx->evalWithCatch(fnVec.back(), nullptr);
+
+    das::Module::Shutdown();
 }
