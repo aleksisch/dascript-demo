71af4486d0d5c24c53900b9af0aedc0a5d34fe74
Churkin Aleksey <lekhachurkin@gmail.com>
2026-01-20 16:32:55 +0300

    add C-api example
    Added example with C-api usage and improved a bit cpp-API example.

--- main.cpp
+++ main.cpp
@@ -8,8 +8,20 @@ int main() {
     das::TextPrinter tout;
     das::ModuleGroup dummyGroup;
     auto program = compileDaScript("hello.das",das::make_smart<das::FsFileAccess>(),tout,dummyGroup,{});
+    if (program->failed()) {
+        printf("File `hello.das` not found.\n");
+        return -1;
+    }
     auto pctx = das::SimulateWithErrReport(program, tout);
+    if (!pctx) {
+        printf("Failed to simulate `hello.das`.\n");
+        return -1;
+    }
     auto fnVec = pctx->findFunctions("main");
+    if (fnVec.empty()) {
+        printf("Function `main` not found.\n");
+        return -1;
+    }
     pctx->evalWithCatch(fnVec.back(), nullptr);
 
     das::Module::Shutdown();
