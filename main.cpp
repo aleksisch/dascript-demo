936477877ebb73874bee1bacb8a5b5abc75cc70f
Churkin Aleksey <lekhachurkin@gmail.com>
2026-02-16 21:34:40 +0300

    add c++ module as an example
    C++ API is not stable, but here's example of simple module.

--- main.cpp
+++ main.cpp
@@ -1,28 +1,44 @@
+#include "daScript/ast/dyn_modules.h"
 #include "daScript/daScript.h"
-#include "daScript/das_common.h"
 
 int main() {
-    NEED_MODULE(Module_BuiltIn)
+    das::TextPrinter tout;
+    auto faccess = das::smart_ptr<das::FsFileAccess>(new das::FsFileAccess);
+
+    NEED_ALL_DEFAULT_MODULES
+    das::require_dynamic_modules(faccess, das::getDasRoot(), "./", tout);
     das::Module::Initialize();
 
-    das::TextPrinter tout;
     das::ModuleGroup dummyGroup;
-    auto program = compileDaScript("hello.das",das::make_smart<das::FsFileAccess>(),tout,dummyGroup,{});
+    das::CodeOfPolicies policies;                        // compiler setup
+
+    // Enables AOT linking.
+    policies.aot = true;
+    // If some functions have changed do not fail.
+    policies.fail_on_no_aot = false;
+
+    auto program = compileDaScript("hello.das",faccess,tout,dummyGroup,policies);
     if (program->failed()) {
-        printf("File `hello.das` not found.\n");
+        for ( auto & err : program->errors ) {
+            tout << reportError(err.at, err.what, err.extra, err.fixme, err.cerr);
+        }
+        tout << "File `hello.das` not found.\n";
         return -1;
     }
-    auto pctx = das::SimulateWithErrReport(program, tout);
-    if (!pctx) {
-        printf("Failed to simulate `hello.das`.\n");
+    das::Context pctx(program->getContextStackSize());
+    if ( !program->simulate(pctx, tout) ) {
+        tout << "failed to simulate\n";
+        for ( auto & err : program->errors ) {
+            tout << reportError(err.at, err.what, err.extra, err.fixme, err.cerr);
+        }
         return -1;
     }
-    auto fnVec = pctx->findFunctions("main");
+    auto fnVec = pctx.findFunctions("main");
     if (fnVec.empty()) {
-        printf("Function `main` not found.\n");
+        tout << "Function `main` not found.\n";
         return -1;
     }
-    pctx->evalWithCatch(fnVec.back(), nullptr);
+    pctx.evalWithCatch(fnVec.back(), nullptr);
 
     das::Module::Shutdown();
 }
--- modules/moduleHello/hello_module.c
+++ modules/moduleHello/hello_module.c
@@ -1,8 +1,8 @@
 #include "daScript/daScriptC.h"
 #include <stdio.h>
 
-vec4f hello_from_cpp_module( das_context * ctx, das_node * node, vec4f * args ) {
-    return das_result_string("Hello from cpp module");
+vec4f hello_from_c_module( das_context * ctx, das_node * node, vec4f * args ) {
+    return das_result_string("Hello from c module!");
 }
 
 #ifdef _MSC_VER
@@ -16,7 +16,7 @@ extern "C" {
     EXPORT_API das_module * register_dyn_Module_Hello() {
         das_module * mod = das_module_create ("HelloModule");
         das_module_group * lib = das_modulegroup_make();
-        das_module_bind_interop_function(mod, lib, &hello_from_cpp_module, "hello_from_cpp_module", "hello_from_cpp_module", SIDEEFFECTS_modifyExternal, "s ");
+        das_module_bind_interop_function(mod, lib, &hello_from_c_module, "hello_from_c_module", "hello_from_c_module", SIDEEFFECTS_modifyExternal, "s ");
         das_modulegroup_release(lib);
         return mod;
     }
