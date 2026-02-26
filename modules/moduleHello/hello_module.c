2795a1581c6a4e364b16023d1838a2171e226184
Boris Batkin <bbatkin@gmail.com>
2026-02-25 19:33:27 -0800

    Fix build issues with C module and AOT paths
    - hello_module.c: Remove extern C wrapper (invalid in C source files)
      and fix indentation
    - moduleHelloCPP/CMakeLists.txt: Add WORKING_DIRECTORY and use absolute
      paths in AOT custom command so the build works from any directory.
      Use DAS::daslang instead of DAS::daslang_dyn (renamed in install)

--- modules/moduleHello/hello_module.c
+++ modules/moduleHello/hello_module.c
@@ -12,12 +12,10 @@ vec4f hello_from_c_module( das_context * ctx, das_node * node, vec4f * args ) {
 #endif
 #define EXPORT_API DAS_EXPORT_DLL
 
-extern "C" {
-    EXPORT_API das_module * register_dyn_Module_Hello() {
-        das_module * mod = das_module_create ("HelloModule");
-        das_module_group * lib = das_modulegroup_make();
-        das_module_bind_interop_function(mod, lib, &hello_from_c_module, "hello_from_c_module", "hello_from_c_module", SIDEEFFECTS_modifyExternal, "s ");
-        das_modulegroup_release(lib);
-        return mod;
-    }
+EXPORT_API das_module * register_dyn_Module_Hello() {
+    das_module * mod = das_module_create ("HelloModule");
+    das_module_group * lib = das_modulegroup_make();
+    das_module_bind_interop_function(mod, lib, &hello_from_c_module, "hello_from_c_module", "hello_from_c_module", SIDEEFFECTS_modifyExternal, "s ");
+    das_modulegroup_release(lib);
+    return mod;
 }
